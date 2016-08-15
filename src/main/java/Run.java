d5338b2b45fafea88cd4d98d2434d8a689043b45
Oleg Gondar <Oleg_Gondar@epam.com>
2016-08-15 15:02:00 +0300

    Initial commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Oleg_Gondar on 8/15/2016.
+ */
+public class Run {
+
+
+    public static boolean buildOrNotBuild(boolean pass){
+        if(pass){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/TestBuildPass.java
@@ -0,0 +1,18 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Oleg_Gondar on 8/15/2016.
+ */
+public class TestBuildPass {
+
+    @Test
+    public void checkPass(){
+
+
+    assertTrue(Run.buildOrNotBuild(true));
+
+    }
+}
